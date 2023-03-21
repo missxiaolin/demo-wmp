ba95f1578b38e52e3feecc43734e20f31a23aa94
462441355@qq.com <462441355@qq.com>
2023-03-21 14:09:36 +0800

    * 小程序生成海报

--- pages/example/index.js
+++ pages/example/index.js
@@ -1,66 +1,165 @@
-// pages/example/index.js
+import Card from '../../palette/card';
+
 Page({
+    imagePath: '',
+    history: [],
+    future: [],
+    isSave: false,
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        template: {},
+        customActionStyle: {
+            border: {
+                borderColor: '#1A7AF8',
+            },
+            scale: {
+                textIcon: '/palette/switch.png',
+                imageIcon: '/palette/scale.png',
+            },
+            delete: {
+                icon: '/palette/close.png',
+            },
+        },
+    },
 
-  },
+    onImgOK(e) {
+        this.imagePath = e.detail.path;
+        this.setData({
+            image: this.imagePath,
+        });
+        if (this.isSave) {
+            this.saveImage(this.imagePath);
+        }
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+    onRevert() {
+        const pre = this.history.pop();
+        if (!pre) {
+            return;
+        }
+        const needRefresh = pre.index && pre.index >= 0 && pre.index <= this.data.template.views.length;
+        if (needRefresh) {
+            if (this.data.template.views[pre.index].id === pre.view.id) {
+                this.data.template.views.splice(pre.index, 1);
+            } else {
+                this.data.template.views.splice(pre.index, 0, pre.view);
+            }
+            this.future.push(pre);
+        } else {
+            for (let i in this.data.template.views) {
+                if (this.data.template.views[i].id === pre.view.id) {
+                    this.future.push({
+                        view: this.data.template.views[i]
+                    });
+                    this.data.template.views[i] = pre.view;
+                    break;
+                }
+            }
+        }
+        const props = {
+            paintPallette: this.data.template,
+        };
+        if (needRefresh) {
+            props.template = this.data.template;
+        } else {
+            props.action = pre;
+        }
+        this.setData(props);
+    },
 
-  },
+    onRecover() {
+        const fut = this.future.pop();
+        if (!fut) {
+            return;
+        }
+        const needRefresh = fut.index && fut.index >= 0 && fut.index <= this.data.template.views.length;
+        if (needRefresh) {
+            if (this.data.template.views[fut.index].id === fut.view.id) {
+                this.data.template.views.splice(fut.index, 1);
+            } else {
+                this.data.template.views.splice(fut.index, 0, fut.view);
+            }
+            this.history.push(fut);
+        } else {
+            for (let i in this.data.template.views) {
+                if (this.data.template.views[i].id === fut.view.id) {
+                    this.history.push({
+                        view: this.data.template.views[i]
+                    });
+                    this.data.template.views[i] = fut.view;
+                    break;
+                }
+            }
+        }
+        const props = {
+            paintPallette: this.data.template,
+        };
+        if (needRefresh) {
+            props.template = this.data.template;
+        } else {
+            props.action = fut;
+        }
+        this.setData(props);
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    saveImage() {
+        if (this.imagePath && typeof this.imagePath === 'string') {
+            this.isSave = false;
+            wx.saveImageToPhotosAlbum({
+                filePath: this.imagePath,
+            });
+        }
+    },
 
-  },
+    touchEnd({
+        detail
+    }) {
+        let needRefresh = detail.index >= 0 && detail.index <= this.data.template.views.length;
+        if (needRefresh) {
+            this.history.push({
+                ...detail,
+            });
+            if (this.data.template.views[detail.index].id === detail.view.id) {
+                this.data.template.views.splice(detail.index, 1);
+            } else {
+                this.data.template.views.splice(detail.index, 0, detail.view);
+            }
+        } else {
+            if (!this.data.template || !this.data.template.views) {
+                return;
+            }
+            for (let view of this.data.template.views) {
+                if (view.id === detail.view.id) {
+                    this.history.push({
+                        view: {
+                            ...detail.view,
+                            ...view,
+                        },
+                    });
+                    view.css = detail.view.css;
+                    break;
+                }
+            }
+        }
+        this.future.length = 0;
+        const props = {
+            paintPallette: this.data.template,
+        };
+        if (needRefresh) {
+            props.template = this.data.template;
+        }
+        this.setData(props);
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
-})
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+        this.setData({
+            paintPallette: new Card().palette(),
+        });
+    },
+});
