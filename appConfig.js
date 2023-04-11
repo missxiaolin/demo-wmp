c7f82e3d1bd419b0a4e2ac5441f80e0c7f27b68a
462441355@qq.com <462441355@qq.com>
2023-04-11 10:43:26 +0800

    * 导航栏

--- appConfig.js
+++ appConfig.js
@@ -1,40 +1,41 @@
 module.exports = {
-    "pages": [
-        "pages/signName/index",
-        "pages/main/main",
-        "pages/swiperY/index",
-        "pages/swiperX/index",
-        "pages/example/index",
-        "pages/index/index",
-        "pageB/index/index",
-        "pages/userCenter/index",
-        "pages/community/community"
-    ],
-    "requiredPrivateInfos": [
-        "chooseLocation"
-    ],
-    "tabBar": {
-        "custom": true,
-        "list": [{
-                "pagePath": "pages/main/main",
-                "text": "认养"
-            },
-            {
-                "pagePath": "pages/community/community",
-                "text": "社区"
-            },
-            {
-                "pagePath": "pages/userCenter/index",
-                "text": "我的"
-            }
-        ]
-    },
-    "window": {
-        "backgroundTextStyle": "light",
-        "navigationBarBackgroundColor": "#fff",
-        "navigationBarTitleText": "",
-        "navigationBarTextStyle": "black"
-    },
-    "style": "v2",
-    "sitemapLocation": "sitemap.json"
+  "pages": [
+    "pages/nav/index",
+    "pages/signName/index",
+    "pages/main/main",
+    "pages/swiperY/index",
+    "pages/swiperX/index",
+    "pages/example/index",
+    "pages/index/index",
+    "pageB/index/index",
+    "pages/userCenter/index",
+    "pages/community/community"
+  ],
+  "requiredPrivateInfos": [
+    "chooseLocation"
+  ],
+  "tabBar": {
+    "custom": true,
+    "list": [{
+        "pagePath": "pages/main/main",
+        "text": "认养"
+      },
+      {
+        "pagePath": "pages/community/community",
+        "text": "社区"
+      },
+      {
+        "pagePath": "pages/userCenter/index",
+        "text": "我的"
+      }
+    ]
+  },
+  "window": {
+    "backgroundTextStyle": "light",
+    "navigationBarBackgroundColor": "#fff",
+    "navigationBarTitleText": "",
+    "navigationBarTextStyle": "black"
+  },
+  "style": "v2",
+  "sitemapLocation": "sitemap.json"
 }
